9c48853b08452c3212d2ee779d36450414c50e67
Ngo Quoc Dat <datlechin@gmail.com>
2021-11-24 15:51:41 +0700

    run build dist files

--- extend.php
+++ extend.php
@@ -15,9 +15,7 @@ use Flarum\Extend;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js'),
-    (new Extend\Frontend('admin'))
-        ->js(__DIR__.'/js/dist/admin.js'),
+        ->js(__DIR__ . '/js/dist/forum.js'),
 
-    new Extend\Locales(__DIR__.'/locale')
+    new Extend\Locales(__DIR__ . '/locale'),
 ];
