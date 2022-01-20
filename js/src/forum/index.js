d89931913167ca83e88dd4e64ef1f9ab6ab8f850
Ngo Quoc Dat <datlechin@gmail.com>
2022-01-20 17:51:05 +0700

    chore: flarum-cli audit infra --fix

--- js/src/forum/index.js
+++ js/src/forum/index.js
@@ -1,6 +1,7 @@
+import app from 'flarum/forum/app';
 import addIgnoreControls from './addIgnoreControls';
 import addLikeControls from './addLikeControls';
-import addMarkAsReadControls from './addMarkasReadControls';
+import addMarkAsReadControls from './addMarkAsReadControls';
 
 app.initializers.add('datlechin/flarum-add-like-controls', () => {
   addMarkAsReadControls();
